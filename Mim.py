e3501041c907eba8caa8eb1f18f3eb2af7784671
Jahim Sankoh <jahimsankoh319@gmail.com>
2024-12-18 15:41:03 -0500

    Update Mim.py

--- Mim.py
+++ Mim.py
@@ -5,4 +5,15 @@ root.title("Mim")
 
 root.geometry("440x620")
 
+tabView = customtkinter.CTkTabView(app)
+tabView.pack(padx=20, pady=20)
+
+tabView.add("Grid Browser")
+tabView.add("Channel Browser")
+tabView.add("Favourites")
+tabView.add("Preferences")
+tabView.add("Search")
+tabView.add("Remote")
+tabView.add("About")
+
 root.mainloop()
