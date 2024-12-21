38011886967722a89c13f1ad3ca7a277843e6dbd
Jahim Sankoh <jahimsankoh319@gmail.com>
2024-12-21 13:11:59 -0500

    Update Mim.py

--- Mim.py
+++ Mim.py
@@ -1,30 +1,29 @@
-import customtkinter as ctk
+import customtkinter
 
-class TabViewApp(ctk.CTk):
+
+class MyTabView(customtkinter.CTkTabview):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+
+        
+        self.add("Grid Browser")
+        self.add("Channel Browser")
+        self.add("Favourites")
+        self.add("Preferences")
+        self.add("Search")
+        self.add("Remote")
+        self.add("About")
+
+        
+
+
+class App(customtkinter.Mim):
     def __init__(self):
         super().__init__()
 
-        self.title("Mim")
-        self.geometry("440x620")
+        self.tab_view = MyTabView(master=self)
+        self.tab_view.grid(row=0, column=0, padx=20, pady=20)
 
-self.tab_view = ctk.CTkTabView(self)
-self.tab_view.pack(fill="both", expand=True, padx=20, pady=20)
 
-self.tab_view.add("Grid Browser")
-self.tab_view.add("Channel Browser")
-self.tab_view.add("Favourites")
-self.tab_view.add("Preferences")
-self.tab_view.add("Search")
-self.tab_view.add("Remote")
-self.tab_view.add("About")
-
-self.tab_view.set("Preferences")
-
-def disable_fullscreen():
-    app.attributes("-fullscreen", False)
-
-app.bind("<Escape>", lambda event: disable_fullscreen())
-
-if __name__ == "__main__":
-    app = TabViewApp()
-    app.mainloop()
+app = App()
+app.mainloop()
