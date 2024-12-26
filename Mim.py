696530a5d86fb4a7798cbcf0db7143e220623606
Jahim Sankoh <jahimsankoh319@gmail.com>
2024-12-26 03:53:55 -0500

    Update Mim.py

--- Mim.py
+++ Mim.py
@@ -3,6 +3,7 @@ import customtkinter as ctk
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
+        
         self.resizable(False, False)
         self.title("Mim")
         self.geometry("730x620")
