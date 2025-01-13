bd8b8bc422c0fd6d8c341feaa7f935d08a6df6b6
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-01-13 13:39:47 -0500

    Update Mim.py

--- Mim.py
+++ Mim.py
@@ -19,6 +19,26 @@ class App(ctk.CTk):
         self.tab5 = self.tabview.add("Search")
         self.tab6 = self.tabview.add("Remote")
         self.tab7 = self.tabview.add("About")
+        
+        self.tabview.set("Preferences")
+        
+        for button in self.tabview._segmented_button._buttons_dict.values():
+            button.configure(width=100, height=50)
+        self.tab1_frame = self.tabview.tab("Grid Browser")
+        self.tab2_frame = self.tabview.tab("Channel Browser")
+        self.tab3_frame = self.tabview.tab("Favourites")
+        self.tab4_frame = self.tabview.tab("Preferences")
+        self.tab5_frame = self.tabview.tab("Search")
+        self.tab6_frame = self.tabview.tab("Remote")
+        self.tab7_frame = self.tabview.tab("About")
+        
+        self.tab1_frame.pack(padx=20, pady=20)
+        self.tab2_frame.pack(padx=20, pady=20)
+        self.tab3_frame.pack(padx=20, pady=20)
+        self.tab4_frame.pack(padx=20, pady=20)
+        self.tab5_frame.pack(padx=20, pady=20)
+        self.tab6_frame.pack(padx=20, pady=20)
+        self.tab7_frame.pack(padx=20, pady=20)
 
 if __name__ == "__main__":
     app = App()
