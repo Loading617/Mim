5e40bd81bd3813115d8e921a1569b1f57fe1973f
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-02-25 12:27:18 -0500

    Update Mim.py

--- Mim.py
+++ Mim.py
@@ -1,6 +1,6 @@
 import customtkinter as ctk
 
-class App(ctk.CTk):
+class Mim(ctk.CTk):
     def __init__(self):
         super().__init__()
         
@@ -8,7 +8,10 @@ class App(ctk.CTk):
         self.title("Mim")
         self.iconbitmap("Mim.ico")
         self.geometry("440x620")
-
+        
+        self.tabview = ctk.CTkTabview(self)
+        self.tabview.pack(fill="both", expand=True, padx=10, pady=10)
+        
         self.tab1 = self.tabview.add("Grid Browser")
         self.tab2 = self.tabview.add("Channel Browser")
         self.tab3 = self.tabview.add("Favourites")
@@ -18,8 +21,9 @@ class App(ctk.CTk):
         self.tab7 = self.tabview.add("About")
         
         self.tabview.set("Preferences")
-        
 
 if __name__ == "__main__":
-    app = App()
+    ctk.set_appearance_mode("dark")
+    ctk.set_default_color_theme("blue")
+    app = Mim()
     app.mainloop()
