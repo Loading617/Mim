2ad5d36cc0b6bad0dbe393ea5bd45e1c5798799f
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-02-28 00:57:52 -0500

    Update Mim.py

--- Mim.py
+++ Mim.py
@@ -1,29 +1,21 @@
 import customtkinter as ctk
 
-class Mim(ctk.CTk):
-    def __init__(self):
-        super().__init__()
-        
-        self.resizable(False, False)
-        self.title("Mim")
-        self.iconbitmap("Mim.ico")
-        self.geometry("440x620")
-        
-        self.tabview = ctk.CTkTabview(self)
-        self.tabview.pack(fill="both", expand=True, padx=10, pady=10)
-        
-        self.tab1 = self.tabview.add("Grid Browser")
-        self.tab2 = self.tabview.add("Channel Browser")
-        self.tab3 = self.tabview.add("Favourites")
-        self.tab4 = self.tabview.add("Preferences")
-        self.tab5 = self.tabview.add("Search")
-        self.tab6 = self.tabview.add("Remote")
-        self.tab7 = self.tabview.add("About")
-        
-        self.tabview.set("Preferences")
+ctk.set_appearance_mode("dark")
 
-if __name__ == "__main__":
-    ctk.set_appearance_mode("dark")
-    ctk.set_default_color_theme("blue")
-    app = Mim()
-    app.mainloop()
+app = ctk.CTk()
+
+app.resizable(False, False)
+app.title("Mim")
+app.iconbitmap("Mim.ico")
+app.geometry("440x620")
+
+tabview = ctk.CTkTabview(app)
+tabview.pack(pady=20, padx=20, fill="both", expand=True)
+
+tab1 = tabview.add("     Grid Browser     ")
+tab2 = tabview.add("     Channel Browser     ")
+tab3 = tabview.add("     Favourites      ")
+tab4 = tabview.add("     Preferences     ")
+tab5 = tabview.add("     Search   ")
+
+app.mainloop()
